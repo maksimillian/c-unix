fd1ed891d6c14e1587551e117a89b1a14d89fcf5
student <arsenij18@gmail.com>
2018-06-11 13:01:11 +0300

    reverse lines

---
+++ reverseLines/revLines.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+FILE *tmpF;
+int main(int argc, char const *argv[]) {
+  tmpF = fopen("tmpF", "w+");
+  char s[256];
+  int c;
+  while (fgets(s, 256, stdin) != NULL) {
+    fputs(s, tmpF);
+  }
+  fclose(tmpF);
+  tmpF = fopen("tmpF", "r");
+  fseek(tmpF, -2L, SEEK_END);
+  c = fgetc(tmpF);
+  fputc(c, stdout);
+  fclose(tmpF);
+}
